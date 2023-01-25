61b5bb7137ab92fe256c0c1a1e859a93c157e595
ibrahimayuba393 <ibrahimayuba393@gmail.com>
2023-01-25 17:36:10 +0100

    init

--- controllers/moralis.js
+++ controllers/moralis.js
@@ -3,7 +3,24 @@ const { EvmChain } = require('@moralisweb3/common-evm-utils');
 const { BadRequestError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
-async function runApp() {
+// async function runApp() {
+//   await Moralis.start({
+//     apiKey: 'JhNUZ4tjmZv0IBmbh98KFEF3HephX5UmDfPfG2aPwuj1rFtcPMV9lBRGxoWZDlxn',
+//   });
+
+//   const address = '0xe68626f79b5C4CFA9686A091d3303E4c624DFeCf';
+
+//   const chain = EvmChain.BSC;
+
+//   const response = await Moralis.EvmApi.token.getTokenPrice({
+//     address,
+//     chain,
+//   });
+
+//   console.log(response.toJSON().usdPrice);
+// }
+
+async function getCoinPrice(req, res) {
   await Moralis.start({
     apiKey: 'JhNUZ4tjmZv0IBmbh98KFEF3HephX5UmDfPfG2aPwuj1rFtcPMV9lBRGxoWZDlxn',
   });
@@ -17,13 +34,7 @@ async function runApp() {
     chain,
   });
 
-  return response.toJSON().usdPrice;
-}
-
-async function getCoinPrice(req, res) {
-  const coinprice = await runApp();
-
-  res.status(StatusCodes.OK).json({ coinprice });
+  res.status(StatusCodes.OK).json({ coinprice: response.toJSON().usdPrice });
 }
 
 module.exports = {
